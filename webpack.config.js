e028e8c35b095be3ddce03540f0d19be6834aae4
ClareMcDonald <clare.s.mcdonald@gmail.com>
2023-05-26 16:30:44 -0700

    installed babel-loader and added babel configs

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,20 @@ module.exports = {
     filename: 'app.bundle.js',
     path: __dirname + '/dist',
   },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: __dirname + '/src/index.html',
