edaaa2d41f2fe16e37b329d42cb94a221e17c0ca
ClareMcDonald <clare.s.mcdonald@gmail.com>
2023-05-26 15:47:55 -0700

    installed html-webpack-plugin and added it to webpack.conf

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,16 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  entry: ['./src/index.js'],
+  entry: ['./ src / index.js'],
+  output: {
+    clean: true,
+    filename: 'app.bundle.js',
+    path: __dirname + '/dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/index.html',
+    }),
+  ],
 };
